7af8d7e8237095b3cb4ca6eca4e4848fa0870fad
Lazar <lazardemin@gmail.com>
2020-11-30 11:50:46 -0500

    fix for docs

--- docs/conf.py
+++ docs/conf.py
@@ -54,6 +54,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# explicitly assign master document to index.rst - https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
